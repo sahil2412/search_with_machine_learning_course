847f23947d26df4ea05f98117836e7ff5f4dfe01
sahil2412 <khan.sahil2412@gmail.com>
2023-11-20 08:29:35 +0530

    Solution for week3

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import csv
+import re
 
 # Useful if you want to perform stemming.
 import nltk
@@ -49,8 +50,29 @@ queries_df = pd.read_csv(queries_file_name)[['category', 'query']]
 queries_df = queries_df[queries_df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+def normalisation(text):
+    return ' '.join(stemmer.stem(word) for word in re.sub(r'[^a-zA-Z0-9]', ' ', str.lower(text)).split())
+
+# Unit test
+print(normalisation("Beats By Dr. Dre- Monster Pro Over-the-Ear Headphones -") == "beat by dr dre monster pro over the ear headphon")
+
+queries_df['query'] = queries_df['query'].apply(normalisation)
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+value_counts = queries_df['category'].value_counts().reset_index()
+value_counts.columns = ['category', 'count']
+
+
+while min(value_counts['count']) < min_queries:
+    merged_queries_df = queries_df.merge(value_counts, on='category', how='left').merge(parents_df, on='category', how='left')
+    merged_queries_df['category'] = merged_queries_df.apply(
+        lambda row: row['parent'] if row['count'] < min_queries else row['category'], axis=1)
+    queries_df = merged_queries_df[['category', 'query']].copy()
+    value_counts = queries_df['category'].value_counts().reset_index()
+    value_counts.columns = ['category', 'count']
+
+
+print("Number of unique categories: ", queries_df['category'].nunique())
 
 # Create labels in fastText format.
 queries_df['label'] = '__label__' + queries_df['category']
